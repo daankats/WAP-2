6b702f5cf2ca793d94e9443071867e63d4a623f3
Daan Kats <info@viadev.nl>
2023-06-03 18:49:28 +0200

    no message

--- views/home.php
+++ views/home.php
@@ -11,3 +11,9 @@ use app\core\App;
     } else {
         echo 'Guest';
     } ?></h3>
+
+  <h3 style="color:red;">Let op inlog formulier zit soms onder de pagina, nog een kleine bug.</h3> 
+   <p>
+   Om de database te configureren, dien je de inhoud van het bestand .env.example te bekijken en op basis daarvan een nieuw bestand genaamd .env aan te maken met de juiste gegevens. Daarna dien je het bestand migrations.php uit te voeren met PHP om de database en tabellen aan te maken.
+Vervolgens moet je een account aanmaken in de database en deze voorzien van de rol 'beheerder'. Met dit account kun je andere gebruikers aanmaken.
+    </p>
