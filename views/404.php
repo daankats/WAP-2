424e061eb553a6ac01ae14bfdd454b3c9e89f21f
Daan Kats <info@viadev.nl>
2023-06-09 12:26:46 +0200

    fixed button on 404 page

--- views/404.php
+++ views/404.php
@@ -1,3 +1,3 @@
 <h1>404 - Pagina niet gevonden</h1>
     <p>De pagina die u probeerd te bezoeken bestaat niet</p>
-    <button class="btn btn-sm btn-danger" >Naar Home</button>
+    <a href="/" class="btn btn-danger">Home</a>
