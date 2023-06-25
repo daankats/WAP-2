77bc11e4834b1a1b5490bfffa82aefd7d8cde78f
Daan Kats <info@viadev.nl>
2023-06-25 17:05:09 +0200

    added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'daankats/wap-2',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '71f6d329fa7fe025346da974d3587bdca6e9efb8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'daankats/wap-2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '71f6d329fa7fe025346da974d3587bdca6e9efb8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
